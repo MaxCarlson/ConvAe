8a8e2396f0de833594f8e8e46621a367bd16deb9
Maxwell Carlson <bart4282819@gmail.com>
2021-05-16 19:12:39 -0700

    Trained model

--- ConvAe/ConvAe.py
+++ ConvAe/ConvAe.py
@@ -2,6 +2,7 @@ import numerapi
 import numpy as np
 import pandas as pd
 import keras as K
+import matplotlib.pyplot as plt
 from keras import models, layers
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
@@ -41,33 +42,69 @@ testX, testY = load_mnist('data', kind='t10k')
 trainX = np.divide(trainX, 255)
 testX = np.divide(testX, 255)
 
-def addBlock(model, filters, kernel, encoder=True):
-    model.add(layers.Conv2D(filters, kernel, padding='same') if encoder
-              else layers.Conv2D(filters, kernel, padding='same'))
-    model.add(layers.ReLU())
-    model.add(layers.MaxPool2D(padding='same') if encoder 
-              else layers.UpSampling2D())
+input = K.Input(shape=(28, 28, 1))
 
-model = models.Sequential()
-model.add(layers.Input((28,28,1)))
-addBlock(model, 32, (5,5))
-addBlock(model, 16, (3,3))
-addBlock(model, 8, (2,2))
-model.add(layers.Flatten())
-model.add(layers.Dense(4))
-model.add(layers.Dense(2))
-model.add(layers.Dense(4))
-model.add(layers.Reshape((2,2,1)))
-addBlock(model, 8, (2,2), False)
-addBlock(model, 16, (3,3), False)
-addBlock(model, 32, (5,5), False)
-model.add(layers.Conv2D(1, (1,1)))
-model.add(layers.Activation(K.activations.sigmoid))
+x = layers.Conv2D(16, (3, 3),  padding='same')(input)
+x = layers.Activation('relu')(x)
+x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.Conv2D(8, (3, 3),  padding='same')(x)
+x = layers.Activation('relu')(x)
+x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.Conv2D(8, (3, 3),  padding='same')(x)
+x = layers.Activation('relu')(x)
+x = layers.MaxPooling2D((2, 2), padding='same')(x)
+
+x = layers.Flatten()(x)
+x = layers.Dense(4)(x)
+x = layers.Activation('relu')(x)
+encoded = layers.Dense(2)(x)
+x = layers.Dense(4)(encoded)
+x = layers.Activation('relu')(x)
+x = layers.Reshape((2,2,1))(x)
+
+x = layers.UpSampling2D((2, 2))(x)
+x = layers.Conv2D(8, (3, 3),  padding='same')(x)
+x = layers.Activation('relu')(x)
+x = layers.UpSampling2D((2, 2))(x)
+x = layers.Conv2D(8, (3, 3),  padding='same')(x)
+x = layers.Activation('relu')(x)
+x = layers.UpSampling2D((2, 2))(x)
+x = layers.Conv2D(16, (3, 3))(x)
+x = layers.Activation('relu')(x)
+x = layers.UpSampling2D((2, 2))(x)
+decoded = layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+model = K.Model(input, decoded)
+
+
+model.summary()
 
 model.compile(optimizer=K.optimizers.Adam(learning_rate=0.001), 
         #loss=keras.losses.mean_squared_error,
         loss=K.losses.binary_crossentropy,
         metrics=[])
 
-model.fit(trainX, trainX, 32, 1)
+stopping = K.callbacks.EarlyStopping(
+        monitor="val_loss",
+        patience=10,
+        mode="auto",
+        baseline=None,
+        min_delta=0.0005,
+        restore_best_weights=True)
+hist = model.fit(trainX, trainX, 256, 100, validation_data=(testX, testX), 
+                 callbacks=[stopping])
+model.save('models/m')
+
+def plotLoss(name, history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    plt.savefig(name + '_loss.jpg')
+    plt.close()
+
+plotLoss('training', hist)
 
