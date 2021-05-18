f94935f4a46516a3b0c19084c9efad6e770cffbe
Maxwell Carlson <bart4282819@gmail.com>
2021-05-17 17:58:42 -0700

    Lots of updates

--- ConvAe/ConvAe.py
+++ ConvAe/ConvAe.py
@@ -1,4 +1,4 @@
-import numerapi
+import os
 import numpy as np
 import pandas as pd
 import keras as K
@@ -7,11 +7,12 @@ from keras import models, layers
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
-#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 def load_mnist(path, kind='train'):
     import os
@@ -42,35 +43,56 @@ testX, testY = load_mnist('data', kind='t10k')
 trainX = np.divide(trainX, 255)
 testX = np.divide(testX, 255)
 
+def addNoise(x):
+    ret = np.copy(x)
+    for i in range(len(x)):
+        ret[i] += np.random.normal(0, 1, (28,28,1))
+    return ret
+
+# Add gaussian noise
+trainXn = addNoise(trainX)
+testXn = addNoise(testX)
+
+plt.imshow(np.reshape(testX[3], (28,28)), cmap='gray')
+plt.show()
+
 input = K.Input(shape=(28, 28, 1))
 
 x = layers.Conv2D(16, (3, 3),  padding='same')(input)
 x = layers.Activation('relu')(x)
 x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.BatchNormalization()(x)
 x = layers.Conv2D(8, (3, 3),  padding='same')(x)
 x = layers.Activation('relu')(x)
 x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.BatchNormalization()(x)
 x = layers.Conv2D(8, (3, 3),  padding='same')(x)
 x = layers.Activation('relu')(x)
 x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.BatchNormalization()(x)
 
 x = layers.Flatten()(x)
 x = layers.Dense(4)(x)
 x = layers.Activation('relu')(x)
+x = layers.BatchNormalization()(x)
 encoded = layers.Dense(2)(x)
 x = layers.Dense(4)(encoded)
 x = layers.Activation('relu')(x)
+x = layers.BatchNormalization()(x)
 x = layers.Reshape((2,2,1))(x)
 
 x = layers.UpSampling2D((2, 2))(x)
 x = layers.Conv2D(8, (3, 3),  padding='same')(x)
 x = layers.Activation('relu')(x)
+x = layers.BatchNormalization()(x)
 x = layers.UpSampling2D((2, 2))(x)
 x = layers.Conv2D(8, (3, 3),  padding='same')(x)
 x = layers.Activation('relu')(x)
+x = layers.BatchNormalization()(x)
 x = layers.UpSampling2D((2, 2))(x)
 x = layers.Conv2D(16, (3, 3))(x)
 x = layers.Activation('relu')(x)
+x = layers.BatchNormalization()(x)
 x = layers.UpSampling2D((2, 2))(x)
 decoded = layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
@@ -86,14 +108,24 @@ model.compile(optimizer=K.optimizers.Adam(learning_rate=0.001),
 
 stopping = K.callbacks.EarlyStopping(
         monitor="val_loss",
-        patience=10,
+        patience=75,
         mode="auto",
         baseline=None,
         min_delta=0.0005,
         restore_best_weights=True)
-hist = model.fit(trainX, trainX, 256, 100, validation_data=(testX, testX), 
-                 callbacks=[stopping])
-model.save('models/m')
+
+# Training
+#
+batchSize = 2048
+#hist = model.fit(trainXn, trainX, batchSize, 750, validation_data=(testXn, testX), 
+#                 callbacks=[stopping])
+
+#hist = model.fit(trainX, trainX, batchSize, 750, validation_data=(testX, testX), 
+#                 callbacks=[stopping])
+#model.save('models/noise')
+#model = K.models.load_model('models/m')
+model = K.models.load_model('models/noise')
+
 
 def plotLoss(name, history):
     plt.plot(history.history['loss'])
@@ -106,5 +138,42 @@ def plotLoss(name, history):
     plt.savefig(name + '_loss.jpg')
     plt.close()
 
-plotLoss('training', hist)
+#plotLoss('training', hist)
+
+
+# Test image reconstruction
+idxs = [0,1,2,6]
+testCount = 4
+
+# Reconstruct normal image
+#out = model.predict(testX[idxs])
+#fig, axs = plt.subplots(2, testCount)
+#for i in range(2):
+#    for ji, j in enumerate(idxs):
+#        axs[i,ji].imshow(np.reshape(out[ji] if i != 0 else testX[j], 
+#                                   (28,28)), cmap='gray')
+
+# Reconstructed noise images
+out = model.predict(testXn[idxs])
+fig, axs = plt.subplots(3, testCount)
+for i in range(3):
+    for ji, j in enumerate(idxs):
+        if i == 0:
+            axs[i,ji].imshow(np.reshape(testX[j], (28,28)), cmap='gray')
+        elif i == 1:
+            axs[i,ji].imshow(np.reshape(testXn[j], (28,28)), cmap='gray')
+        else:
+            axs[i,ji].imshow(np.reshape(out[ji], (28,28)), cmap='gray')
+
+plt.show()
+plt.savefig('recon_noise.jpg')
+
+#plt.imshow(np.reshape(out[0], (28,28)), cmap='gray')
+#plt.show()
+#plt.imshow(np.reshape(out[1], (28,28)), cmap='gray')
+#plt.show()
+#plt.imshow(np.reshape(out[2], (28,28)), cmap='gray')
+#plt.show()
+
+
 
